5134c47814d700a3927ad0edad110407244a2692
faizantoheed456 <faizantoheed456@gmail.com>
2026-06-01 17:41:24 +0500

    Python 100 solved excercises

---
+++ program_12.py
@@ -0,0 +1,5 @@
+"""Write a program that calculates the kinetic 
+energy of a moving object, using the formula 
+E = (mv²) / 2, where E is the kinetic energy, 
+m is the mass of the object, and v is the velocity."""
+
--- program_8.py
+++ program_8.py
@@ -1,1 +1,27 @@
+"""Write a program that calculates the delta of a quadratic
+equation (Δ = b² - 4ac)."""
 
+
+def get_number(prompt):
+	"""Returns the valid data for processing"""
+	
+	while True:
+		try:
+			return float(input(prompt))
+		except ValueError:
+			print("Try to enter a number")
+			
+def delta_quadratic_equation(a, b, c):
+	"""Calculates the delta of a quadratic equation"""
+	
+	return (b**2) - (4*a*c)
+	
+	
+	
+a = get_number("Enter the coefficient a: ")
+b = get_number("Enter the coefficient b: ")
+c = get_number("Enter the coefficient c: ")
+
+delta = delta_quadratic_equation(a, b, c)
+
+print(f"Δ = {delta:.2f}") 
